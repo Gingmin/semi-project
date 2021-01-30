9c64133462a32d41119cbac77e583357b2aad85e
Gingmin <kkmk220@gmail.com>
2021-01-30 15:17:16 +0900

    마스터 업뎃
    마스터 업뎃

---
+++ web/views/regist/passwordChangeSettings.js
@@ -0,0 +1,8 @@
+//인증번호를 쳐야지만 확인 버튼이 눌리는 로직
+$(document).ready(function() {
+         		$('.number_input').on('keyup', function() {
+         			var flag = true;
+         			flag = $(this).val().length > 0 ? false : true;
+         			$('.number_confirm').attr('disabled', flag);
+         		});
+         	});
