04a485783674cf2003ca1d0d215410ca65fbe363
SEC <SEC@DESKTOP-JL5GILM>
2021-02-12 13:45:33 +0900

    트레이너 마이페이지,매출내역,클래스 추가하기 연결완료!

---
+++ src/com/greedy/semi/trainer/controller/TrainerPageServlet.java
@@ -0,0 +1,24 @@
+package com.greedy.semi.trainer.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/trainer/page")
+public class TrainerPageServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String path = "/WEB-INF/views/trainer-page/mypage_trainer.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- web/resources/js/event.js
+++ web/resources/js/event.js
@@ -28,9 +28,27 @@ window.onload = function() {
 		}
 	}
 
-	
-	
-	
+	/* 트레이너 마이 페이지*/
+	if(document.getElementById("trainer")) {
+		const $logout = document.getElementById("trainer");
+		$logout.onclick = function() {
+			location.href = "/semi/trainer/page";
+		}
+	}
+	/* 클래스 개설하기 */
+	if(document.getElementById("insert_class")) {
+		const $logout = document.getElementById("insert_class");
+		$logout.onclick = function() {
+			location.href = "/semi/trainer/class";
+		}
+	}
+	/* 매출 내역 */
+	if(document.getElementById("sales-details")) {
+		const $logout = document.getElementById("sales-details");
+		$logout.onclick = function() {
+			location.href = "/semi/trainer/sales_details";
+		}
+	}
 	
 	
 	
