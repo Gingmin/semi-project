b4f864a31512270857eebb552a370d708a812ef1
SEC <SEC@DESKTOP-JL5GILM>
2021-02-23 01:43:40 +0900

    2021-02-23 민서 업데이트

---
+++ src/com/greedy/semi/trainer/controller/TrainerProfileImgEditServlet.java
@@ -0,0 +1,18 @@
+package com.greedy.semi.trainer.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/trainer/profileEdit")
+public class TrainerProfileImgEditServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
