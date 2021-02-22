7956d4b64c682fe54874b7bb22de7fbc68f30c5c
SEC <SEC@DESKTOP-JL5GILM>
2021-02-23 02:28:16 +0900

    2021-02-23- 민서 02시 27분 업데이트

--- src/com/greedy/semi/trainer/controller/TrainerProfileImgEditServlet.java
+++ src/com/greedy/semi/trainer/controller/TrainerProfileImgEditServlet.java
@@ -1,18 +1,148 @@
 package com.greedy.semi.trainer.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.greedy.semi.member.model.dto.MemberDTO;
+import com.greedy.semi.trainer.dto.AttachmentDTO;
+import com.greedy.semi.trainer.dto.ClassDTO;
+
+import net.coobird.thumbnailator.Thumbnails;
+
 @WebServlet("/trainer/profileEdit")
 public class TrainerProfileImgEditServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		if(ServletFileUpload.isMultipartContent(request)) {
+			
+			String rootLocation = getServletContext().getRealPath("/");
+			int maxFileSize = 1024 * 1024 * 10;
+			String encodingType = "UTF-8";
+			
+			String fileUploadDirectory = rootLocation + "/resources/upload/original";
+			String thumbnailDirectory = rootLocation + "/resources/upload/thumbnail";
+			
+			File directory1 = new File(fileUploadDirectory);
+			File directory2 = new File(thumbnailDirectory);
+			
+			if(!directory1.exists() || !directory2.exists()) {
+				System.out.println("원본 저장 폴더 생성 : " + directory1.mkdirs());
+				System.out.println("썸네일 저장 폴더 생성 : " + directory2.mkdirs());
+			}
+			
+			Map<String, String> parameter = new HashMap<>();
+			List<Map<String, String>> fileList = new ArrayList<>();
+			
+			DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
+			fileItemFactory.setRepository(new File(fileUploadDirectory));
+			fileItemFactory.setSizeThreshold(maxFileSize);
+			
+			ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory);
+			
+			try {
+				List<FileItem> fileItems = fileUpload.parseRequest(request);
+				
+				for(FileItem item : fileItems) {
+					System.out.println(item);
+				}
+				
+				for(int i = 0; i < fileItems.size(); i++) {
+					
+					FileItem item = fileItems.get(i);
+					
+					if(!item.isFormField()) {
+						if(item.getSize() > 0) {
+							String fieldName = item.getFieldName();
+							String originFileName = item.getName();
+							
+							int dot = originFileName.lastIndexOf(".");
+							String ext = originFileName.substring(dot);
+							
+							String randomFileName = UUID.randomUUID().toString().replace("-", "") + ext;
+							
+							File storeFile = new File(fileUploadDirectory + randomFileName);
+							
+							item.write(storeFile);
+							
+							Map<String, String> fileMap = new HashMap<>();
+							fileMap.put("fieldName", fieldName);
+							fileMap.put("originFileName", originFileName);
+							fileMap.put("savedFileName", randomFileName);
+							fileMap.put("savePath", fileUploadDirectory);
+							
+							int width = 0;
+							int height = 0;
+							if("profileEdit".equals(fieldName)) {
+								fileMap.put("fileType", "profileEdit");
+								
+									width = 180;
+									height = 120;
+							} else {
+								fileMap.put("fileType", "profileEdit");
+									
+									width = 180;
+									height = 120;
+							}
+							
+							Thumbnails.of(fileUploadDirectory + randomFileName)
+									.size(width, height)
+									.toFile(thumbnailDirectory + "thumbnail_" + randomFileName);
+							
+							fileMap.put("thumbnailPath", "resources/upload/thumbnail/thumbnail_" + randomFileName);
+							
+							fileList.add(fileMap);
+						}
+					} else {
+						
+						parameter.put(item.getFieldName(), new String(item.getString().getBytes("ISO-8859-1"), "UTF-8"));
+					}
+				}
+				
+				System.out.println("parameter : " + parameter);
+				System.out.println("fileList : " + fileList);
+				
+				ClassDTO thumbnail = new ClassDTO();
+				thumbnail.setTrainerNo(((MemberDTO) request.getSession().getAttribute("loginMember")).getNo());
+				
+				thumbnail.setAttachmentList(new ArrayList<AttachmentDTO>());
+				
+			} catch (FileUploadException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
 		doGet(request, response);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
